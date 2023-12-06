f74e955797378cfea1cd83d21960650e62d8f63d
Asgirox <115622345+Asgirox@users.noreply.github.com>
2023-12-06 14:14:23 +0200

    Update 3.cpp

--- pz9/3.cpp
+++ pz9/3.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
 using namespace std;
 
-int fiveth(int numbers[], int size) {
-	cout << "Fiveth task: " << endl;
-	int max = numbers[0];
-	int index_max = 0;
-	for (int i = 0; i < size; i++) {
-		if (max < numbers[i]) {
-			max = numbers[i];
-		}
-	}
-	int max2 = numbers[0];
-	for (int i = 0; i < size; i++) {
-		if (numbers[i] != max && max2 < numbers[i]) {
-			max2 = numbers[i];
-		}
-	}
-	
-	cout << "The second bigger number: " << max2;
-	cout << endl << endl;
-	return 0;
+int main() {
+    
+    const int size = 5;
+    int arr[size];
+    srand(time(0));
+    
+    for(int i = 0; i < size; i++) {
+        arr[i] = rand() % 100 + 1; 
+    }
+    
+    cout << "Початковий масив: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+    sort(arr, arr + size);
+
+    int secondLargest = arr[size - 2];
+
+    cout << "Другий за величиною елемент: " << secondLargest << endl;
+
+    return 0;
 }
