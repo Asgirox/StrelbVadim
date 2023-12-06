e3789f58e47cca5996488017a0fdfbaa591c6ebe
Asgirox <115622345+Asgirox@users.noreply.github.com>
2023-12-06 14:12:55 +0200

    2.cpp

--- pz9/2.cpp
+++ pz9/2.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
-int fourth(int numbers[], int size) {
-	
-	cout << "Fourth task: " << endl;
-	int A;
-	cout << "Enter a number A: ";
-	cin >> A;
-	int count = 0;
-	for (int i = 0; i < size; i++) {
-		if (numbers[i] > A) {
-			count++;
-			
-		}
-	}
-	if (count == 0) {
-		cout << "There is noone bigger number than A";
-	}
-	else {
-		cout << count;
-	}
-	cout << endl<<endl;
-	return 0;
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    
+    const int size = 8;
+    int arr[size];
+    int A;
+    int count = 0;
+    srand(time(0));
 
+    cout << "Введіть число А: ";
+    cin >> A;
+
+    for(int i = 0; i < size; i++) {
+        arr[i] = rand() % 100 + 1; 
+    }
+    
+    cout << "Початковий масив: ";
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for(int i = 0; i < size; i++) {
+        if(arr[i] > A) {
+            count++;
+        }
+    }
+
+    cout << "Кількість елементів, більша за " << A << ": " << count << endl;
+
+    return 0;
 }
