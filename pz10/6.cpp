92f3b7ce72b24e2de6ec1546871de4316c852e35
Asgirox <115622345+Asgirox@users.noreply.github.com>
2023-12-20 15:12:36 +0200

    Update 6.cpp

--- pz10/6.cpp
+++ pz10/6.cpp
@@ -1,27 +1,44 @@
 #include <iostream>
+#include <iomanip>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
+
+    
 	int const m = 4;
 	int const n = 7;
-	int numbers[m][n];
+	int array[m][n];
+	srand(time(0));
 
+	cout << "Початковий масив:" << endl;
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
-			numbers[i][j] = rand() % 10;
-			cout << numbers[i][j] << ' ';
+			array[i][j] = rand() % 50 + 1;
+			cout << setw(3) << array[i][j] << " ";
 		}
 		cout << endl;
 	}
-	int min = 0;
+	
+	cout << "\n" << endl;
+	
+	int min = 50;
+	int row = 0;
+	int column = 0;
+	
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
-			if (numbers[i][j] < min) {
-				min = numbers[i][j];
+			if (array[i][j] < min) {
+				min = array[i][j];
+				row = i + 1;
+				column = j + 1;
 			}
 		}
 	}
-	cout << "min number in an array are: " << min<<endl;
+	cout << "Мінімальне значення у масиві: " << min << endl;
+	cout << "Ряд: " << row << endl;
+	cout << "Стовпчик: " << column << endl;
+	
 	return 0;
 }
