bdf6fb39df08e9b7d1e6c0ae1d5da9084e13c4f2
Asgirox <115622345+Asgirox@users.noreply.github.com>
2024-06-07 15:02:24 +0300

    Add files via upload

---
+++ 2 Семестр с++/Лр 23/1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+const int DivideByZero = 111;
+
+float div(float arg1, float arg2)
+{
+  if(arg2 == 0.0)
+    throw DivideByZero;
+  return arg1 / arg2;
+} 
+
+int main(void)
+{
+  float r, a, b;
+  while(cin >> a)
+  {
+    cin >> b;
+    try
+    {
+      r=div(a,b);
+      cout << r << endl;
+    }
+    catch(const int e)
+    {      
+      cout << "Are you kidding me?" << endl;
+    }    
+  }
+  return 0;
+}
